7887538314ed090987edc0ec7024098fcfa6d274
derek hecker <derekhecker8@gmail.com>
2020-06-20 11:02:48 -0400

    Initial Commit, No content

--- StudentClass/Program.cs
+++ StudentClass/Program.cs
@@ -6,7 +6,7 @@ namespace StudentClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worlds!");
         }
     }
 }
